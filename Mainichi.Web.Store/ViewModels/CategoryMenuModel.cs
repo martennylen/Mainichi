7bc7034193b8710f7ea0868d9b074931ddb2a587
Marten Nylen <c.marten.nylen@gmail.com>
2013-11-23 20:14:49 +0100

    Menurenderer now partial view.

---
+++ Mainichi.Web.Store/ViewModels/CategoryMenuModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Mainichi.Web.Store.ViewModels
+{
+    public class CategoryMenuModel
+    {
+        public IEnumerable<Category> Categories { get; set; }
+        public string Identifier { get; set; }
+    }
+}
