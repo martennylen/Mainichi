6e1c3b8400034e146c47b6be3fb9cf5ba6e86a43
Marten Nylen <c.marten.nylen@gmail.com>
2013-11-28 15:05:12 +0100

    Added GA.

--- Mainichi.Web.Store/Controllers/HomeController.cs
+++ Mainichi.Web.Store/Controllers/HomeController.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-using Mainichi.Web.Store.App_Start;
-using Mainichi.Web.Store.Filters;
+﻿using System.Web.Mvc;
 using Mainichi.Web.Store.ViewModels;
 
 namespace Mainichi.Web.Store.Controllers
