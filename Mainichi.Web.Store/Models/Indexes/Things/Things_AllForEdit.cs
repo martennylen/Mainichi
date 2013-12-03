858e6d34353b2fbff804dc6b35507a09c88221c3
Marten Nylen <c.marten.nylen@gmail.com>
2013-12-03 21:00:49 +0100

    Admin. Admin.

---
+++ Mainichi.Web.Store/Models/Indexes/Things/Things_AllForEdit.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Mainichi.Web.Store.ViewModels;
+using Microsoft.Ajax.Utilities;
+using Raven.Abstractions.Indexing;
+using Raven.Client.Indexes;
+
+namespace Mainichi.Web.Store.Models.Indexes
+{
+    public class Things_AllForEdit : AbstractIndexCreationTask<Thing>
+    {
+        public Things_AllForEdit()
+        {
+            Map = things => from thing in things
+                select new { thing.Id, thing.Name };
+
+            Index(x => x.Id, FieldIndexing.Analyzed);
+            Index(x => x.Name, FieldIndexing.Analyzed);
+        }
+    }
+}
---
+++ Mainichi.Web.Store/ViewModels/FeaturedProductsViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Mainichi.Web.Store.ViewModels
+{
+    public class FeaturedProductsViewModel
+    {
+        public IEnumerable<Thing> AllThings { get; set; }
+        public IEnumerable<Thing> FeaturedThings { get; set; } 
+    }
+}
